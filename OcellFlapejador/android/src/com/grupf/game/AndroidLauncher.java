5195120db11fc3b672c2f7c1b4ac133b6083d407
Joan Sanmartí Escudero <joan.sanmarti@enti.cat>
2019-05-10 09:37:21 +0200

    CreacióProjecte

---
+++ OcellFlapejador/android/src/com/grupf/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.grupf.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.grupf.game.OcellFlapejador;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OcellFlapejador(), config);
+	}
+}
---
+++ OcellFlapejador/desktop/src/com/grupf/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.grupf.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grupf.game.OcellFlapejador;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OcellFlapejador(), config);
+	}
+}
